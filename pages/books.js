49e17c48c46be389d36335b7259089b82205697e
Graham Marlow <mgmarlow@hey.com>
2021-02-17 20:32:18 -0800

    Redo book page

--- pages/books.js
+++ pages/books.js
@@ -11,16 +11,53 @@ export async function getStaticProps() {
   }
 }
 
-// TODO:
+function BookItem({ book }) {
+  return (
+    <div>
+      <h3 className="is-size-4">
+        <i>{book.title}</i>
+      </h3>
+      <p className="subtitle">{book.author}</p>
+    </div>
+  )
+}
+
+function BookList({ books }) {
+  return (
+    <ul>
+      {books.map(book => (
+        <li className="mb-6" key={book.id}>
+          <BookItem book={book} />
+        </li>
+      ))}
+    </ul>
+  )
+}
+
 export default function Books({ books }) {
+  const completedBooks = books.filter(book => book.state === 'completed')
+  const newBooks = books.filter(book => book.state === 'new')
+
   return (
     <Layout>
       <main>
-        <ul>
-          {books.map(book => (
-            <li>{book.title}</li>
-          ))}
-        </ul>
+        <div className="container">
+          <section className="section">
+            <h1 className="is-size-1 title">Books</h1>
+
+            <div className="my-3">
+              <h2 className="is-size-2">Completed</h2>
+              <hr />
+              <BookList books={completedBooks} />
+            </div>
+
+            <div>
+              <h2 className="is-size-2">New</h2>
+              <hr />
+              <BookList books={newBooks} />
+            </div>
+          </section>
+        </div>
       </main>
     </Layout>
   )
