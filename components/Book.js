87a59283c1b87479e1bc9fac7bcd6aa424fc59b7
Graham Marlow <mgmarlow@hey.com>
2021-02-17 20:58:12 -0800

    Add fallback url

--- components/Book.js
+++ components/Book.js
@@ -2,15 +2,13 @@ import classnames from 'classnames'
 import Image from 'next/image'
 import styles from './Book.module.sass'
 
+const FALLBACK_URL = 'https://bulma.io/images/placeholders/320x480.png'
+
 export default function Book({ book }) {
   return (
     <div className={classnames('is-inline-block', styles.book)}>
       <div className="has-text-centered">
-        <Image
-          height={240}
-          width={160}
-          src="https://bulma.io/images/placeholders/320x480.png"
-        />
+        <Image height={240} width={160} src={book.img || FALLBACK_URL} />
       </div>
 
       <div className="content is-small">
