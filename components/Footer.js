7338c9cca1ce0413a52ab3562b7ee28fd747a27a
Graham Marlow <mgmarlow@gmail.com>
2021-02-28 09:24:59 -0800

    Add footer links

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,46 @@
+import Link from 'next/link'
+import { DISCORD_INVITE_URL } from '../constants/env'
+
+const FooterLink = ({ href, children }) => (
+  <li>
+    <Link href={href}>
+      <a className="has-text-dark">{children}</a>
+    </Link>
+  </li>
+)
+
 export default function Footer() {
   return (
     <footer className="footer">
       <div className="content has-text-centered">
         <p>
-          <strong>Tech Book Club</strong> by Graham Marlow. Check out the{' '}
-          <a href="https://github.com/mgmarlow/tech-book-club">source code</a>.
+          <strong>Tech Book Club</strong> by Graham Marlow. Source code licensed
+          by{' '}
+          <a
+            className="has-text-dark"
+            href="https://github.com/mgmarlow/tech-book-club/blob/master/LICENSE"
+          >
+            GPL v3.0
+          </a>
+          .
         </p>
+      </div>
+
+      <div className="columns is-centered">
+        <div className="column is-narrow mr-5">
+          <ul>
+            <FooterLink href="/about">about</FooterLink>
+            <FooterLink href="/books">books</FooterLink>
+          </ul>
+        </div>
+        <div className="column is-narrow">
+          <ul>
+            <FooterLink href="https://github.com/mgmarlow/tech-book-club">
+              source ↗
+            </FooterLink>
+            <FooterLink href={DISCORD_INVITE_URL}>discord ↗</FooterLink>
+          </ul>
+        </div>
       </div>
     </footer>
   )
---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Layout from '../components/Layout'
+
+export default function About() {
+  return (
+    <Layout>
+      <main>
+        <div className="container">
+          <div className="section">
+            <h1 className="is-size-1 title">About</h1>
+
+            <div className="content">
+              <p>Coming soon!</p>
+            </div>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  )
+}
