a7cfe8abe0c76d3c136e274666de518a93e28ec9
Graham Marlow <mgmarlow@hey.com>
2021-03-30 17:03:22 -0700

    Add FAQ

--- pages/index.js
+++ pages/index.js
@@ -6,60 +6,37 @@ import Nav from '../components/Nav'
 import Book from '../components/Book'
 import { getAllBooks } from '../lib/books'
 
-const AboutSection = () => {
+export async function getStaticProps() {
+  const currentBook = getAllBooks().find(book => book.state === 'in_progress')
+
+  return {
+    props: {
+      currentBook,
+    },
+  }
+}
+
+export default function Home({ currentBook }) {
   return (
-    <section className="section">
-      <div className="content is-medium">
-        <h2>What's it all about?</h2>
-        <p>
-          Learning is more fun with friends. Book club is an opportunity to
-          apply theory, challenge ideas, and study in a friendly environment.
-        </p>
-        <p>
-          Join our weekly discussion sessions held on{' '}
-          <strong>Wednesdays at 4:30 PST</strong>. All you need to get started
-          is a few questions and a copy of the book.
-        </p>
-      </div>
-    </section>
+    <>
+      <Nav />
+      <SEO />
+
+      <main>
+        <div className="container is-thin mb-6">
+          <Hero currentBook={currentBook} />
+          <AboutSection />
+          <AddBookSection />
+          <FAQ />
+        </div>
+      </main>
+
+      <Footer />
+    </>
   )
 }
 
-const AddBookSection = () => {
-  return (
-    <section className="section">
-      <div className="content is-medium">
-        <h2>Want to suggest a book?</h2>
-        <p>
-          Submit a pull request to{' '}
-          <a href="https://github.com/mgmarlow/tech-book-club/blob/master/lib/books.json">
-            books.json
-          </a>
-          . Here are some guidelines:
-        </p>
-        <ul>
-          <li>
-            <p>
-              suggest books you <em>want</em> to read, not ones you{' '}
-              <em>should</em> read
-            </p>
-          </li>
-          <li>
-            <p>choose books that relate to software development</p>
-          </li>
-          <li>
-            <p>
-              consider free books, or ones available on{' '}
-              <a href="https://learning.oreilly.com/home/">O'Reilly</a>
-            </p>
-          </li>
-        </ul>
-      </div>
-    </section>
-  )
-}
-
-const Hero = ({ currentBook }) => {
+function Hero({ currentBook }) {
   return (
     <section className="section">
       <div className="columns">
@@ -69,7 +46,7 @@ const Hero = ({ currentBook }) => {
 
             {currentBook ? (
               <p>
-                Currently reading: <em>The Imposter's Handbook</em>
+                Currently reading: <em>{currentBook.title}</em>
               </p>
             ) : (
               <p>Let's talk about what we're reading.</p>
@@ -101,31 +78,108 @@ const Hero = ({ currentBook }) => {
   )
 }
 
-export async function getStaticProps() {
-  const currentBook = getAllBooks().find(book => book.state === 'in_progress')
-
-  return {
-    props: {
-      currentBook,
-    },
-  }
+function AboutSection() {
+  return (
+    <section className="section">
+      <div className="content is-medium">
+        <h2>What's it all about?</h2>
+        <p>
+          Learning is more fun with friends. Book club is an opportunity to
+          apply theory, challenge ideas, and study in a friendly environment.
+        </p>
+        <p>
+          Join our weekly discussion sessions held on{' '}
+          <strong>Wednesdays at 4:30 PST</strong>. All you need to get started
+          is a few questions and a copy of the book.
+        </p>
+      </div>
+    </section>
+  )
 }
 
-export default function Home({ currentBook }) {
+function FAQ() {
   return (
-    <>
-      <Nav />
-      <SEO />
+    <section className="section">
+      <div className="content is-medium">
+        <h2 id="faq">FAQ</h2>
 
-      <main>
-        <div className="container is-thin mb-6">
-          <Hero currentBook={currentBook} />
-          <AboutSection />
-          <AddBookSection />
+        <div className="mt-3 mb-6">
+          <h5>What goes into a discussion?</h5>
+          <p>
+            We begin discussions with a brief (5-10 minutes) summary of the
+            reading. Afterwards, we ask questions aimed at improving our
+            understanding of the author's main points.
+          </p>
+          <p>
+            For more technical books, we use{' '}
+            <a href="https://gist.github.com/">gists</a> to review code and
+            discuss practical applications. Check out{' '}
+            <a href="https://gist.github.com/mgmarlow/1bc04546aba5e71508fb5a916560e5e1">
+              some
+            </a>{' '}
+            <a href="https://gist.github.com/mgmarlow/b2cc4a07349ed1e67b44f0f17a815a0b">
+              examples
+            </a>{' '}
+            from past discussions.
+          </p>
         </div>
-      </main>
 
-      <Footer />
-    </>
+        <div className="my-6">
+          <h5>Can I still attend the discussion if I missed the reading?</h5>
+          <p>
+            Yes! Your contributions are still valuable even if you missed a
+            chapter or two.
+          </p>
+        </div>
+
+        <div className="my-6">
+          <h5>
+            A billion note-taking apps came out in 2020. What do the cool kids
+            use?
+          </h5>
+          <p>
+            <a href="https://notion.so/">Notion</a> is my personal favorite, but
+            some other great alternatives are{' '}
+            <a href="https://obsidian.md/">Obsidian</a>,{' '}
+            <a href="https://foambubble.github.io/foam/">Foam</a>, and{' '}
+            <a href="https://www.zettlr.com/">Zettlr</a>.
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+function AddBookSection() {
+  return (
+    <section className="section">
+      <div className="content is-medium">
+        <h2>Want to suggest a book?</h2>
+        <p>
+          Submit a pull request to{' '}
+          <a href="https://github.com/mgmarlow/tech-book-club/blob/master/lib/books.json">
+            books.json
+          </a>
+          . Here are some guidelines:
+        </p>
+        <ul>
+          <li>
+            <p>
+              suggest books you <em>want</em> to read, not ones you{' '}
+              <em>should</em> read
+            </p>
+          </li>
+          <li>
+            <p>choose books that relate to software development</p>
+          </li>
+          <li>
+            <p>
+              consider free books, or ones available on{' '}
+              <a href="https://learning.oreilly.com/home/">O'Reilly</a>
+            </p>
+          </li>
+        </ul>
+      </div>
+    </section>
   )
 }
