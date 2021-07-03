b9ff65d2596054aa91bac70225e0b1e278c5c76f
Graham Marlow <mgmarlow@gmail.com>
2021-07-03 09:53:33 -0700

    fixup! Add firebase

--- constants/env.js
+++ constants/env.js
@@ -1,2 +1,7 @@
 export const DISCORD_INVITE_URL =
   'mailto: mgmarlow@hey.com?subject=Request to Join Tech Book Club'
+
+export const FIREBASE_API_KEY = process.env.NEXT_PUBLIC_FIREBASE_API_KEY
+export const FIREBASE_PROJECT_ID = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+export const FIREBASE_SENDER_ID = process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID
+export const FIREBASE_APP_ID = process.env.NEXT_PUBLIC_FIREBASE_APP_ID
