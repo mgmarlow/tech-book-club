772200033c6a44cb76e90c99631015eb1233ac8f
Graham Marlow <mgmarlow@gmail.com>
2021-07-05 10:20:32 -0700

    Add discord invite link

--- constants/env.js
+++ constants/env.js
@@ -1,5 +1,4 @@
-export const DISCORD_INVITE_URL =
-  'mailto: mgmarlow@hey.com?subject=Request to Join Tech Book Club'
+export const DISCORD_INVITE_URL = 'https://discord.gg/hxvHgqswNs'
 
 export const FIREBASE_API_KEY = process.env.NEXT_PUBLIC_FIREBASE_API_KEY
 export const FIREBASE_PROJECT_ID = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
