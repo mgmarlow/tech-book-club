c98134146677a97df74b54fc421dbbe8832a4fb2
Graham Marlow <mgmarlow@hey.com>
2021-08-19 17:26:16 -0700

    Link to newsletter archive

--- constants/env.js
+++ constants/env.js
@@ -1,7 +1,7 @@
 // Outbound URLs
 export const LICENSE_URL =
   'https://github.com/mgmarlow/tech-book-club/blob/master/LICENSE'
-export const NEWSLETTER_URL = 'https://buttondown.email/techbookclub'
+export const NEWSLETTER_URL = 'https://buttondown.email/techbookclub/archive/'
 
 // ENV
 export const FIREBASE_API_KEY = process.env.NEXT_PUBLIC_FIREBASE_API_KEY
