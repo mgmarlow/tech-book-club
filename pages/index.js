37808cd70e6b335b10bbb0b01f49622674f69ca9
Graham Marlow <mgmarlow@hey.com>
2021-08-24 19:12:23 -0700

    Add articles to landing

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,11 @@ import Nav from '../components/Nav'
 import SEO from '../components/SEO'
 import BookList from '../components/BookList'
 import { getAllBooks } from '../lib/books'
-import styles from './index.module.sass'
 import { getArticleDataByDate } from '../lib/articles'
 
 export async function getStaticProps() {
   const currentBook = getAllBooks().find(book => book.state === 'in_progress')
-  const articleData = await getArticleDataByDate()
+  const articleData = (await getArticleDataByDate()).slice(0, 3)
 
   return {
     props: {
@@ -74,71 +73,27 @@ function Hero({ currentBook }) {
 function ArticlePreview({ articleData }) {
   const previews = articleData.map(a => (
     <Link href={`/archive/${a.id}`} key={a.id}>
-      <li>{a.title}</li>
+      <li className="media is-clickable">
+        <div className="media-content">
+          <span>
+            <strong>{a.title}</strong>
+          </span>
+          <p className="is-size-6">{a.description}</p>
+        </div>
+      </li>
     </Link>
   ))
 
   return (
     <section className="section">
       <div className="content is-medium">
-        <h2>Browse the archive</h2>
-
-        <ul>{previews}</ul>
-      </div>
-    </section>
-  )
-}
-
-function NewsletterForm() {
-  return (
-    <section className="section" id="newsletter">
-      <div className="content is-medium">
-        <h2>Join the newsletter</h2>
+        <h2>Recent articles</h2>
 
         <p>
-          <b>tech book club</b> explores classic computer science literature and
-          applies it to the everyday craft. Each newsletter is packed full of
-          notes, code samples, and analysis related to that month's book.
+          Or, browse the <Link href="/archive">archive</Link>.
         </p>
 
-        <p>Read along or just tune in to learn something new:</p>
-
-        <div>
-          <form
-            action="https://buttondown.email/api/emails/embed-subscribe/techbookclub"
-            method="post"
-            target="popupwindow"
-            onSubmit={() =>
-              window.open(
-                'https://buttondown.email/techbookclub',
-                'popupwindow',
-              )
-            }
-            className="field has-addons"
-          >
-            <div className={classnames('control', styles.newsletterInput)}>
-              <input
-                placeholder="your@email.com"
-                className="input is-medium"
-                type="email"
-                name="email"
-                id="bd-email"
-              />
-            </div>
-            <input type="hidden" value="1" name="embed" />
-            <div className="control">
-              <input
-                className="button is-primary is-medium"
-                type="submit"
-                value="Subscribe"
-              />
-            </div>
-          </form>
-
-          <p className="is-size-6 has-text-centered">
-            No spam, no trackers, unsubscribe anytime.
-          </p>
-        </div>
+        <ul>{previews}</ul>
       </div>
     </section>
   )
