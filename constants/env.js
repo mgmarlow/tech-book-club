ebfda761e7625c82353fc77b199a9b619b1eae9d
Graham Marlow <mgmarlow@hey.com>
2022-01-16 11:26:06 -0800

    Add fake RSS URL to env (fixme)

--- constants/env.js
+++ constants/env.js
@@ -5,3 +5,5 @@ export const ANCHOR_URL = 'https://anchor.fm/techbookclub'
 export const CONTACT_US_URL = 'https://airtable.com/shrKc6x7sWO4rOOel'
 export const GRAHAM_HOME_URL = 'https://mgmarlow.com'
 export const NOLAN_HOME_URL = 'https://nolansedley.netlify.app'
+// TODO:
+export const RSS_URL = undefined
